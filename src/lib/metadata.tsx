9dcde54aeb30f42524b54c86c199a977e929328d
ceridwenroberts <ceridwen.roberts@gmail.com>
2024-12-18 13:53:35 +0100

    import icons and format metadata file

--- src/lib/metadata.tsx
+++ src/lib/metadata.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
     title: "Spell Check",
-    description: "Magical Thesaurus",
+    description: "Magical Thesaurus for Wizards and Muggles. Data from WizardWorldAPI",
     icons: [
       {
         rel: 'icon',
